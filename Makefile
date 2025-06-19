9b40896ca2c394ff60e6d86c3f2006d3b8153e35
heliopr <helioprorep@gmail.com>
2025-06-19 20:35:33 -0300

    add: projet setup and basic window

---
+++ Makefile
@@ -0,0 +1,17 @@
+CC = gcc
+LIBS = -I/usr/local/include $(shell pkg-config --static --libs glfw3) -I./include/
+CFLAGS = -Wall -O0
+
+SRC=$(wildcard src/*.c)
+
+
+all: clean build
+
+run: build
+	./build
+
+build: $(SRC)
+	$(CC) $^ $(CFLAGS) $(LIBS) -o $@
+
+clean:
+	rm -f build
---
+++ src/main.c
@@ -0,0 +1,44 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4.6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4.6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Collision Simulation", NULL, NULL);
+    if (window == NULL)
+    {
+        printf("Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        printf("Failed to initialize GLAD\n");
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+  
+    glfwTerminate();
+    return 0;
+}
